e5aa97c1180dbb55e8abfec41bf966bc578e495a
admin <m89606209505@yandex.ru>
2025-04-02 21:40:31 +0300

    Correction

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int[] firstArray = new int[]{1, 2, 3};
         double[] secondArray = new double[]{1.57, 7.6d, 9.986};
+        int[] thirdArray = new int[]{371, 31, 80, 97, 77};
 
         for (int i = 0; i < firstArray.length; i++) {
             if (i != 0) {
@@ -17,6 +18,10 @@ public class Main {
             System.out.print(secondArray[i]);
         }
         System.out.println();
+        for (int i = 0; i < thirdArray.length; i++) {
+            System.out.print(thirdArray[i] + ",");
+        }
+        System.out.println();
         for (int i = firstArray.length - 1; i >= 0; i--) {
             if (i != firstArray.length - 1) {
                 System.out.print(", ");
@@ -30,15 +35,20 @@ public class Main {
                 System.out.print(", ");
         }
         System.out.println();
+        for (int i = thirdArray.length - 1; i >= 0; i--) {
+            System.out.print(thirdArray[i] + ",");
+        }
+        System.out.println();
         for (int i = 0; i < firstArray.length; i++) {
             if (firstArray[i] % 2 != 0)
                 firstArray[i] += 1;
         }
         for (int i = 0; i < 3; i++) {
             System.out.print(firstArray[i]);
-            if (i != 2)
-                System.out.print(", ");
-
+            {
+                if (i != 2)
+                    System.out.print(", ");
+            }
         }
     }
 }
